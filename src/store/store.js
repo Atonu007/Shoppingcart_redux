d4bb51314f2a16266bfb3e6f69a0fbdfe181e978
Atonu Shaha <atonushaha@Atonus-MacBook-Air.local>
2024-01-03 00:15:52 +0600

    try 1

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { ConfigureStore, configureStore } from "@reduxjs/toolkit";
+import {  configureStore } from "@reduxjs/toolkit";
 import { cartReducer } from "./cartslice";
 
 export const store = configureStore({
